6b462a8f40c0768903f4eda79266192a7fcbc710
WIZI18 <154735139+WIZI18@users.noreply.github.com>
2024-02-29 16:42:44 +0500

    abc

---
+++ app.js
@@ -0,0 +1,68 @@
+// Given var x = 25 and var y = 7, what is the result of x % y?
+var x = 25
+var y = 7
+console.log("Q:01 "+" "+"("+x % y+")")
+// If var a = 15 and var b = 4, what is the value of a * b after adding 10 to a and subtracting 2 from b?
+var a = 15 
+var b = 4
+a = a + 10
+b = b - 2
+console.log("Q:02 " + " " +"("+ a * b+")")
+// Suppose var c = 12 and var d = 3, calculate the result of c ** d.
+var c = 12 
+var d = 3
+console.log("Q:03 " + " " +"("+ c ** d+")")
+// For var e = 10 and var f = 3, what is the value of e += f * 2?
+var e = 10 
+var f = 3
+e += f * 2
+console.log("Q:04 " + " " +"("+e+")")
+// Considering var g = 20 and var h = 4, what will be the outcome of g / h after adding 5 to g?
+var g = 20 
+var h = 4
+g = g + 5
+console.log("Q:05 " + " " +"("+g/h+")")
+// Given var i = 8 and var j = 3, determine the value of i * j after subtracting 2 from i.
+var i = 8 
+var j = 3
+i = i - 2
+console.log("Q:06 " + " " +"("+i*j+")")
+// If var k = 18 and var l = 5, what is the result of k - l + 2?
+var k = 18 
+var l = 5
+console.log("Q:07 " + " " +"("+(k - l + 2)+")")
+// Suppose var m = 7 and var n = 2, calculate the value of m ** n
+var m = 7 
+var n = 2
+console.log("Q:08 " + " "+"(" +m ** n+")")
+// For var o = 13 and var p = 4, what will be the outcome of o % (p + 2)?
+var o = 13 
+var p = 4
+console.log("Q:09 " + " " +"("+o % (p + 2)+")")
+// Given var q = 16 and var r = 3, determine the result of q * r after dividing q by 2.
+var q = 16 
+var r = 3
+q = q / 2
+console.log("Q:10 " + " " +"("+q * r+")")
+// If var s = 25 and var t = 3, what is the value of s / t after subtracting 1 from s?
+var s = 25 
+var t = 3
+s = s - 1
+console.log("Q:11 " + " " +"("+s / t+")")
+// Suppose var u = 11 and var v = 3, calculate the value of u - v after adding 2 to v.
+var u = 11 
+var v = 3
+v = v + 2
+console.log("Q:12 " + " "+"(" +(u - v)+")")
+// For var w = 22 and var x = 5, what will be the outcome of w + (x ** 2)?
+var w = 22 
+var x = 5
+console.log("Q:13 " + " "+"(" +(w + (x ** 2))+")")
+// Given var y = 19 and var z = 4, determine the result of y % (z + 1).
+var y = 19 
+var z = 4
+console.log("Q:14 " + " " +"("+(y % (z + 1))+")")
+// If var a = 10 and var b = 3, what is the value of a * (b - 2)?
+var a_1 = 10 
+var b_1 = 3
+console.log("Q:15 " + " " +"("+(a_1 * (b_1 - 2))+")")
